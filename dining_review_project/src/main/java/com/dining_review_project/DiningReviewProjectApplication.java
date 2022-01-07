7b6440c350e9ef907b9f604c02f0d7cf622e858e
Donald Rivers <donaldrivers76@gmail.com>
2022-01-07 14:08:32 -0500

    Add files via upload

---
+++ dining_review_project/src/main/java/com/dining_review_project/DiningReviewProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dining_review_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningReviewProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiningReviewProjectApplication.class, args);
+	}
+
+}
---
+++ dining_review_project/src/test/java/com/dining_review_project/DiningReviewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dining_review_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningReviewProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
